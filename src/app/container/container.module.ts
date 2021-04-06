fa85e16bb2a753e47902ec2224ec69fcc8b37484
HazemBZ <hazembenbz@gmail.com>
2021-04-06 23:56:29 +0100

    visualize page structure + routing

--- src/app/container/container.module.ts
+++ src/app/container/container.module.ts
@@ -4,23 +4,29 @@ import {Routes, RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms'
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ChartsModule } from 'ng2-charts';
+import {ChartistModule} from 'ng-chartist';
 
 import { RegisterComponent } from './register/register.component';
 import { LookUpComponent } from './look-up/look-up.component';
+import { VisualizeComponent } from './visualize/visualize.component';
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent},
-  {path: 'lookup', component: LookUpComponent}
+  {path: 'lookup', component: LookUpComponent},
+  {path: 'visualize', component: VisualizeComponent}
 ];
 
 
 @NgModule({
-  declarations: [RegisterComponent, LookUpComponent],
+  declarations: [RegisterComponent, LookUpComponent, VisualizeComponent],
   imports: [
     CommonModule,
     NgbModule,
     RouterModule.forChild(routes),
-    FormsModule
+    FormsModule,
+    ChartsModule,
+    ChartistModule
   ]
 })
 export class ContainerModule { }
