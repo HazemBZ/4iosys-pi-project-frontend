6e045fb90c83388fc17ed14df5995c177c40cceb
HazemBZ <hazembenbz@gmail.com>
2021-04-07 01:38:51 +0100

    rearranged page to visualize temperature and gaz data with two charts each

--- src/app/container/visualize/visualize.component.ts
+++ src/app/container/visualize/visualize.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualizeComponent implements OnInit {
 
+  containerRefs = ["123","alpha bo", 'eat salade']
 
   lineChartData = [{
     label: '# of Votes',
@@ -21,7 +22,7 @@ export class VisualizeComponent implements OnInit {
     scales: {
       yAxes: [{
         ticks: {
-          beginAtZero: true
+          beginAtZero: false
         }
       }]
     },
@@ -89,131 +90,14 @@ export class VisualizeComponent implements OnInit {
     }
   ];
 
-  areaChartData = [{
-    label: '# of Votes',
-    data: [10, 19, 3, 5, 2, 3],
-    borderWidth: 1,
-    fill: true
-  }];
-
-  areaChartLabels = ["2013", "2014", "2014", "2015", "2016", "2017"];
-
-  areaChartOptions = {};
-
-  areaChartColors = [
-    {
-      borderColor: 'rgba(255,99,132,1)',
-      backgroundColor: 'rgba(255,99,132,.2)'
-    }
-  ];
-
-
-  doughnutPieChartData = [
-    {
-      data: [30, 40, 30],
-    }
-  ];
-
-  doughnutPieChartLabels = ["Pink", "Blue", "Yellow"];
-
-  doughnutPieChartOptions = {
-    responsive: true,
-    animation: {
-      animateScale: true,
-      animateRotate: true
-    }
-  };
-
-  doughnutPieChartColors = [
-    {
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)'
-      ]
-    }
-  ];
-
-
-  scatterChartData = [
-    {
-      label: 'First Dataset',
-      data: [{
-          x: -10,
-          y: 0
-        },
-        {
-          x: 0,
-          y: 3
-        },
-        {
-          x: -25,
-          y: 5
-        },
-        {
-          x: 40,
-          y: 5
-        }
-      ],
-      borderWidth: 1
-    },
-    {
-      label: 'Second Dataset',
-      data: [{
-          x: 10,
-          y: 5
-        },
-        {
-          x: 20,
-          y: -30
-        },
-        {
-          x: -25,
-          y: 15
-        },
-        {
-          x: -10,
-          y: 5
-        }
-      ],
-      borderWidth: 1
-    }
-  ];
-
-  scatterChartOptions = {
-    scales: {
-      xAxes: [{
-        type: 'linear',
-        position: 'bottom'
-      }]
-    }
-  };
-
-  scatterChartColors = [
-    {
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)'      ]
-    },
-    {
-      backgroundColor: [
-        'rgba(54, 162, 235, 0.2)'
-      ],
-      borderColor: [
-        'rgba(54, 162, 235, 1)'
-      ]
-    }
-  ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectContainer(index) {
+    let selectedContainer = this.containerRefs[index];
+    console.log(`selected container ${selectedContainer}`)
   }
 
 }
